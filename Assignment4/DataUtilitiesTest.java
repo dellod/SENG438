b5dffdf33a8a03c4ff29cf464e4587cf80dc97a3
Janna <43868772+thursdaysareawesome@users.noreply.github.com>
2020-03-13 13:17:36 -0600

    fixed datautiltiestest

--- Assignment4/DataUtilitiesTest.java
+++ Assignment4/DataUtilitiesTest.java
@@ -2,7 +2,59 @@ package org.jfree.data;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
+import java.util.ArrayList;package org.jfree.data.junit;
+
+import static org.junit.Assert.*;
+
+import java.security.InvalidParameterException;
+
+import org.jfree.data.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class DataUtilitiesTest
+{
+	@Before
+	public void setUp()
+	{
+		DefaultKeyedValues2D testValues = new DefaultKeyedValues2D();
+		values = testValues;
+
+		testValues.addValue(1, 0, 0);
+		testValues.addValue(4, 1, 0);
+
+	}
+	
+	@After
+	public void tearDown()
+	{
+		values = null;
+	}
+	
+	@Test(expected = InvalidParameterException.class)
+	public void testNullDataColumnTotal()
+	{
+		assertEquals(0.0, DataUtilities.calculateColumnTotal(null, 0), 0.0000001d);
+	}
+	
+	@Test
+	public void testValidDataAndColumnColumnTotal()
+	{
+		assertEquals(5.0, DataUtilities.calculateColumnTotal(values, 0), 0.0000001d);
+	}
+	
+	@Test(expected = InvalidParameterException.class)
+	public void testValidDataInvalidColumnColumnTotal()
+	{
+		assertEquals(0.0, DataUtilities.calculateColumnTotal(null, 1), 0.0000001d);
+	}
+	
+	private Values2D values;
+}
+
 import java.util.Arrays;
 import java.util.List;
 import java.security.InvalidParameterException;
